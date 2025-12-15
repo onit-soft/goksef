23f02e1e9d3a79b26bac8ffafa1ecaec8af54083
Dominik <djmmatracki@gmail.com>
2025-12-15 13:30:23 +0100

    fix(xml_schema): omitempty

--- goksef/xml_schema.go
+++ goksef/xml_schema.go
@@ -146,7 +146,7 @@ type FaWiersz struct {
 	P8A         string `xml:"P_8A"`
 	P8B         string `xml:"P_8B"`
 	P9A         string `xml:"P_9A"`
-	P9B         string `xml:"P_9B"`
+	P9B         string `xml:"P_9B,omitempty"`
 	P11         string `xml:"P_11"`
 	// Available vat rates:
 	// 23
